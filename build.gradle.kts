0bb987d1ff7a6e05e145ad372a671b94904ad31a
ewof <marroq@marroq.xyz>
2025-04-17 11:00:32 -0400

    changelog and modrinth/github publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     `maven-publish`
     id("org.sonarqube") version "6.0.1.5171" // Advanced code quality checks
     id("xyz.jpenilla.run-paper") version "2.3.1" // Paper server for testing/hotloading JVM
+    id("com.modrinth.minotaur") version "2.8.7"
+    id("me.modmuss50.mod-publish-plugin") version "0.5.1"
     id("io.papermc.hangar-publish-plugin") version "0.1.3" // publish to hangar
 }
 
@@ -75,7 +77,12 @@ tasks {
 
     runServer {
         downloadPlugins {
-            github("TownyAdvanced", "Towny", "$townyVersion", "towny-$townyVersion.jar") // we can't use the latest release because it's inside a zip.
+            github(
+                "TownyAdvanced",
+                "Towny",
+                "$townyVersion",
+                "towny-$townyVersion.jar"
+            ) // we can't use the latest release because it's inside a zip.
             modrinth("vaultunlocked", "$vaultUnlockedVersion")
         }
         minecraftVersion("$mainMinecraftVersion")
@@ -89,11 +96,11 @@ publishing {
 }
 
 sonar {
-  properties {
-    property("sonar.projectKey", project.name)
-    property("sonar.projectName", project.name)
-    property("sonar.host.url", "https://mvndisonar.formiko.fr")
-  }
+    properties {
+        property("sonar.projectKey", project.name)
+        property("sonar.projectName", project.name)
+        property("sonar.host.url", "https://mvndisonar.formiko.fr")
+    }
 }
 
 tasks.register("echoVersion") {
@@ -115,6 +122,38 @@ tasks.register("echoReleaseName") {
 val versionString: String = version as String
 val isRelease: Boolean = !versionString.contains("SNAPSHOT")
 
+val modrinthVersions = "1.20.1, 1.20.2, 1.20.3, 1.20.4, 1.20.5, 1.20.6, 1.21, 1.21.1, 1.21.2, 1.21.3, 1.21.4"
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("townywaypoints")
+    versionNumber.set(project.version as String)
+    versionType.set("release")
+    versionName.set("${project.name} ${versionNumber.get()}")
+    uploadFile.set(tasks.jar.get().archiveFile)
+    gameVersions.addAll(modrinthVersions.split(",").map { it.trim() })
+    loaders.addAll("paper", "folia")
+    changelog.set(readChangelog())
+
+    syncBodyFrom.set(rootProject.file("README.md").readText())
+}
+
+publishMods {
+    file.set(tasks.jar.get().archiveFile)
+    type.set(me.modmuss50.mpp.ReleaseType.STABLE)
+    modLoaders.addAll("paper", "folia")
+
+    changelog.set(readChangelog())
+
+    github {
+        accessToken.set(System.getenv("TOWNYWAYPOINTS_GITHUB_PAT"))
+        repository.set("Mvndi/TownyWaypoints")
+        commitish.set("master")
+        tagName.set(project.version as String)
+        displayName.set("Version " + tagName.get())
+    }
+}
+
 // Enable for public plugin with a Hangar page only.
 hangarPublish { // ./gradlew publishPluginPublicationToHangar
     publications.register("plugin") {
@@ -122,9 +161,13 @@ hangarPublish { // ./gradlew publishPluginPublicationToHangar
         channel.set(if (isRelease) "Release" else "Snapshot")
         id.set(project.name)
         apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+
+        changelog.set(readChangelog())
+
         platforms {
             register(io.papermc.hangarpublishplugin.model.Platforms.PAPER) {
-                url = "https://github.com/Mvndi/"+project.name+"/releases/download/"+versionString+"/"+project.name+"-"+versionString+".jar"
+                url =
+                    "https://github.com/Mvndi/" + project.name + "/releases/download/" + versionString + "/" + project.name + "-" + versionString + ".jar"
 
                 // Set platform versions from gradle.properties file
                 val versions: List<String> = supportedMinecraftVersions.replace(" ", "").split(",")
@@ -132,4 +175,43 @@ hangarPublish { // ./gradlew publishPluginPublicationToHangar
             }
         }
     }
+}
+
+tasks.register("publishPlugin") {
+    if ((project.version as String).endsWith("-SNAPSHOT"))
+        throw GradleException("Snapshot versions should not be deployed")
+
+    tasks.getByName("publishPluginPublicationToHangar").dependsOn(tasks.shadowJar)
+    tasks.getByName("publishGithub").dependsOn(tasks.shadowJar)
+
+    dependsOn(tasks.publishMods)
+    dependsOn(tasks.publishAllPublicationsToHangar)
+    dependsOn(tasks.modrinth)
+
+    //dependsOn(tasks.syncAllPagesToHangar)
+    dependsOn(tasks.modrinthSyncBody)
+}
+
+tasks.register("readChangelog") {
+    println(readChangelog())
+}
+
+fun readChangelog(): String {
+    val lines = mutableListOf<String>()
+    val version = project.version.toString().substringBefore("-") // remove -SNAPSHOT if present
+
+    var versionFound = false
+    rootProject.file("src/main/resources/Changelog.txt").readLines().forEach {
+        val line = it.trim()
+
+        if (line.startsWith(version))
+            versionFound = true
+        else if (versionFound && !line.startsWith("-"))
+            return@forEach
+
+        if (versionFound && line.startsWith("-"))
+            lines.add(line)
+    }
+
+    return lines.joinToString("\n")
 }
