95d14438b785a12918ae5d784e2bc50267f7c27f
HydrolienF <hydrolien.f@gmail.com>
2026-09-18 20:23:41 +0200

    1.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "net.mvndicraft.townywaypoints"
-version = "1.10.1"
+version = "1.11.0"
 description = "Configurable plot types for Towny that players can teleport between."
 java.sourceCompatibility = JavaVersion.VERSION_21
 val mainMinecraftVersion = "1.21.11"
